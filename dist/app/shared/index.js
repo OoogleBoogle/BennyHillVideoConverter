1c1b5638d65910c9d0706c3723803bb2c12f4d0c
OoogleBoogle <simattfield@gmail.com>
2016-07-09 13:02:32 -0700

    Deleted current dist for prod build

--- dist/app/shared/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('../video-player'));
-__export(require('../search-page'));
-//# sourceMappingURL=index.js.map
