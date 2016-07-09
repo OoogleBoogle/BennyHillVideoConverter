456085fe91d453efced3f28cd91fd79987b4b71f
OoogleBoogle <simattfield@gmail.com>
2016-07-09 12:35:02 -0700

    Removed reference to routes in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
-import { APP_ROUTE_PROVIDER } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
