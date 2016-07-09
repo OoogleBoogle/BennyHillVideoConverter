ffd9f445f88e37bf5ad88aaa9cc68c7d852cc8c3
OoogleBoogle <simattfield@gmail.com>
2016-07-09 13:24:27 -0700

    Setting up firebase

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "AIzaSyDWXgwiTXtvYxB3jaDAU6-0GM9Y9nW2Zm0",
+    authDomain: "benny-hill-video-converter.firebaseapp.com",
+    databaseURL: "https://benny-hill-video-converter.firebaseio.com",
+    storageBucket: "benny-hill-video-converter.appspot.com",
+  })
+]);
 
